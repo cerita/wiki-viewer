f59bd49d7fd6001da302cfafe5068b873d1634ba
Cerita <ceritakelly@gmail.com>
2016-11-14 23:02:07 -0600

    Wikipedia API Search
    HTML, CSS, JS, AJAX.
    Zipline for Free Code Camp curriculum.

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var userInput;
+var wikiUrl;
+
+function getUserInput() {
+  userInput = document.getElementById("searchTerm").value;
+  getWikiData(userInput);
+};
+
+function getWikiData(userInput) {
+  wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + userInput + "&format=json&callback=?";
+
+  $.ajax({
+    type: 'GET',
+    url: wikiUrl,
+    async:false,
+    dataType: 'json',
+    success: function(data) {
+      $("#search-results").html('');
+        for(var i=0; i < data[1].length; i++) {
+          $('#search-results').prepend("<li><a href=" + data[3][i]+ ">" + data[1][i] + "</a><p>" + data[2][i]+ "</p></li>");
+        }
+       // do something with data
+       console.log(data[1][0]);
+      console.log(data[2][0]);
+      console.log(data[3][0]);
+    },
+    error: function(errorMsg) {
+      console.log("Something went wrong.");
+    }
+})
+};
+
+
+window.onload = function() {
+	button = document.getElementById("search-button");
+	button.addEventListener("click", getUserInput);
+}
